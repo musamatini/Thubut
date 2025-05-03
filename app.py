258870ac8d8bd0b562197fcffacd24480e42eea0
Musa Matini <98687449+musamatini@users.noreply.github.com>
2025-05-04 00:32:19 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room
+import eventlet
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('join_call')
+def on_join(data):
+    room = data['room']
+    join_room(room)
+    emit('user_joined', {'msg': 'A user has joined the call.'}, room=room)
+
+@socketio.on('leave_call')
+def on_leave(data):
+    room = data['room']
+    leave_room(room)
+    emit('user_left', {'msg': 'A user has left the call.'}, room=room)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000)
