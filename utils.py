bf71d7cb1a36ed8808946a4d8a8a90846e303b16
musamatini <musamat00@gmail.com>
2025-05-05 00:40:31 +0300

    gg

--- utils.py
+++ utils.py
@@ -1,28 +1,28 @@
 from flask_mail import Message
 from flask import current_app, url_for
 from threading import Thread
-from app import mail 
+# REMOVED: from app import mail # Remove this from the top
 
 def send_async_email(app, msg):
+    # Import mail here, only when the function is called
+    from app import mail
     with app.app_context():
         try:
             mail.send(msg)
         except Exception as e:
+            # Use app.logger for proper logging in Flask
             app.logger.error(f"Failed to send email: {e}")
-
 
 def send_email(subject, recipients, text_body, html_body):
     app = current_app._get_current_object()
     msg = Message(subject, sender=app.config['MAIL_DEFAULT_SENDER'], recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    # Send email in a separate thread to avoid blocking the request
     Thread(target=send_async_email, args=(app, msg)).start()
-
 
 def send_confirmation_email(user):
     token = user.get_email_confirmation_token()
-    confirm_url = url_for('auth.confirm_email', token=token, _external=True)
+    confirm_url = url_for('confirm_email', token=token, _external=True) # Changed route name
     subject = "Confirm Your Email Address for Thubut"
     text_body = f"""Dear {user.fullname},
 
